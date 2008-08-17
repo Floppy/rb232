eb7b1c4c5d2ccbb784aaf0aa7619de76c7d34197
James Smith <james@floppy.org.uk>
2008-08-17 23:17:45 +0100

    make comments in port.h more ruby-like

--- src/port.h
+++ src/port.h
@@ -1,12 +1,10 @@
 #include "ruby.h"
 
-/*
- * Module and class objects
- */
+/* module RB232 */
 extern VALUE RB232;
+
+/*   class Port */
 extern VALUE RB232_Port;
 
-/*
- * def initialize(options = {})
- */
+/*     def initialize(options = {}) */
 VALUE rb232_port_initialize(int argc, VALUE* argv, VALUE self);
