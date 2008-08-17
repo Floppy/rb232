a5ed68b8c0ab00cce4348f7aec8cfdcc9c173b11
James Smith <james@floppy.org.uk>
2008-08-17 23:07:06 +0100

    add initialize function for Port which takes an optional hash of options

--- spec/port_spec.rb
+++ spec/port_spec.rb
@@ -2,8 +2,28 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe RB232::Port do
 
-  it "can be created with default options" do
-    RB232::Port.new.should_not be_nil
+  describe "upon creation" do
+
+    it "can be created with default options" do
+      RB232::Port.new.should_not be_nil
+    end
+
+    it "can be created with a hash of options" do
+      RB232::Port.new({}).should_not be_nil
+    end
+
+    it "takes only one argument" do
+      lambda {
+        RB232::Port.new({},{})
+      }.should raise_error(ArgumentError, "wrong number of arguments (must be 0 or 1)")
+    end
+
+    it "ensures that argument is a hash" do
+      lambda {
+        RB232::Port.new("hello")
+      }.should raise_error(ArgumentError, "argument must be a hash")
+    end
+
   end
 
 end
