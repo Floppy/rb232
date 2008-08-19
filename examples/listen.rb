5f5ac751dacee5a26501dd1542d1a5d0eb1fe095
James Smith <james@floppy.org.uk>
2008-08-19 10:06:23 +0100

    Add example for port and text protocol

---
+++ examples/listen.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+dir = File.dirname(__FILE__) + '/../lib'
+$LOAD_PATH << dir unless $LOAD_PATH.include?(dir)
+
+require 'rb232'
+require 'rb232/text_protocol'
+
+class SimpleClient
+  def update(message)
+    puts("Message received:")
+    puts(message)
+  end
+end
+
+require 'optparse'
+
+# Command-line options - just baud rate and separator for now
+options = {:port => '/dev/ttyS0', :baud_rate => 9600, :separator => "\n"}
+OptionParser.new do |opts|
+  opts.on("-p", "--serial_port SERIAL_PORT", "serial port") do |p|
+    options[:port] = p
+  end  
+  opts.on("-b", "--baud_rate BAUD_RATE", "baud rate") do |b|
+    options[:baud_rate] = b
+  end  
+  opts.on("-s", "--separator SEPARATOR", "message separator character") do |s|
+    options[:separator] = s
+  end  
+end.parse!
+
+port = RB232::Port.new(options[:port], :baud_rate => options[:baud_rate])
+
+protocol = RB232::TextProtocol.new(port, options[:separator])
+client = SimpleClient.new
+protocol.add_observer(client)
+
+# Start receiving messags
+protocol.start
+
+# Wait a while
+sleep(30)
+
+# OK, finish now
+protocol.stop
